8fb15ca8dd22aa3a6f0f1c1309df1658294fd91a
Lorisa Bulosan <lorisab@hawaii.edu>
2023-11-09 13:10:17 -1000

    Completed Parts 1-5

--- HackwichTen/ViewController.swift
+++ HackwichTen/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //Part 3
+    let initialLocation = CLLocation(latitude: 21.36188, longitude: -158.055725)
+    
+    let regionRadius: CLLocationDistance = 10000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Part 3
+        centerMapOnLocation(location: initialLocation)
+        
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098))
+        mapView.addAnnotation(restaurantOne)
     }
 
-
+    //Part 3
+    func centerMapOnLocation(location: CLLocation)
+    {
+        let coordinateRegion = MKCoordinateRegion(center:location.coordinate,latitudinalMeters: regionRadius,longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+        
+    }
+    
+    
+    
 }
 
